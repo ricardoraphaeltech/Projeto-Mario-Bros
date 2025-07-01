c303e3de33bab982a56b0e1acf345a3b5ae91b56
ricardoraphaeltech <riraphaelusa@gmail.com>
2025-07-01 19:44:05 -0300

    adicionei funcoes para validar dados

--- script.js
+++ script.js
@@ -39,3 +39,45 @@ botaoContato.addEventListener('click', function(){
     formularioFaleConosco.style.left = "50%"
     mascaraFormulario.style.visibility = "visible"
 })
+
+// Evento para validar o envio dos dados
+const nome = document.getElementById('nome')
+const telefone = document.getElementById('telefone')
+const email = document.getElementById('email')
+const duvida = document.getElementById('duvida')
+
+function validarNome(){
+    if(nome.value.trim() === "") {
+        alert("Por favor, preencha seu nome!");
+        return false; // impede o envio do formulário
+    } 
+    
+    return true; // permite o envio do formulário
+}
+
+function validarTelefone(){
+    if(telefone.value.trim() === "") {
+        alert("Por favor, preencha seu telefone!");
+        return false;
+    }
+
+    return true;
+}
+
+function validarEmail(){
+    if(email.value.trim() === "") {
+        alert("Por favor, preencha seu e-mail!");
+        return false;
+    }
+
+    return true;
+}
+
+function validarDuvida(){
+    if(duvida.value.trim() === "") {
+        alert("Por favor, especifique o problema ocorrido!");
+        return false;
+    }
+
+    return true;
+}
